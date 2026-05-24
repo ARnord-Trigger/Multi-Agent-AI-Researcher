8a9c26c90fd4df354638aae1eb0428021c1bb719
Anvesh Rai <learner9521@gmail.com>
2026-05-24 15:23:23 +0530

    reqs & env

---
+++ tools.py
@@ -0,0 +1,10 @@
+from langchain.tools import tool 
+import requests
+from bs4 import BeautifulSoup
+from tavily import TavilyClient
+import os 
+from dotenv import load_dotenv
+from rich import print
+load_dotenv()
+
+
